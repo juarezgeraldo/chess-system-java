ffa54d02c5e9014ced4a8d9f18a1021708e80811
Juarez Junior <juarezgeraldosilva@gmail.com>
2020-01-20 12:09:03 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessExecption.java
@@ -0,0 +1,9 @@
+package chess;
+
+public class ChessExecption extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ChessExecption(String msg) {
+		super(msg);
+	}
+}
