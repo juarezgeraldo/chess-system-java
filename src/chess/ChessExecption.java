fbbfc93e758634e0bddc929633dece50b1bf2d0c
Juarez Junior <juarezgeraldosilva@gmail.com>
2020-01-20 12:53:41 -0300

    Moving pieces

--- src/chess/ChessExecption.java
+++ src/chess/ChessExecption.java
@@ -1,6 +1,8 @@
 package chess;
 
-public class ChessExecption extends RuntimeException{
+import boardgame.BoardExeception;
+
+public class ChessExecption extends BoardExeception{
 	private static final long serialVersionUID = 1L;
 
 	public ChessExecption(String msg) {
